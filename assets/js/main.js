f0b8e1832fcae72d8f72da74f58426ebac62a4ec
sajedur99 <sajedur.me@gmail.com>
2022-10-13 17:58:16 +0600

    wooclock upload

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+;(function($) {
+	var App = {
+
+		demoFunctionName: function() {
+
+		}
+	}
+
+	App.init = function() {
+		this.demoFunctionName();
+	}
+
+	$(function() {
+		App.init();
+	});
+}(jQuery));
